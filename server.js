3e9d931b2e256854aae94482eb0cb8a8ca9c6f95
Pilar Buchen <pilarbuchen@gmail.com>
2021-05-01 12:14:38 -0500

    Add heroku part 2

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const { animals } = require("../zookeper/data/animals.json");
 
-//const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3001;
 const app = express();
 
 function filterByQuery(query, animalsArray) {
